79895c2303eda14c6a695ddaca5537f34e838dad
saraelmahrouk <125182593+saraelmahrouk@users.noreply.github.com>
2025-11-11 09:48:27 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -1,19 +1,16 @@
-#ifndef INSTRUCTOR_H
-#define INSTRUCTOR_H
+
+#ifndef MILESTONE01_INSTRUCTOR_H
+#define MILESTONE01_INSTRUCTOR_H
+#include <string>
 
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
-
-
-
-
-
-
-
-
-
-
-
-
-#endif
+class Instructor : public Person {
+private:
+    string department;
+    int experienceYears;
+public:
+    Instructor(string name, int id, string department, int experienceYears );
+    void display();
+};
+#endif //MILESTONE01_INSTRUCTOR_H
